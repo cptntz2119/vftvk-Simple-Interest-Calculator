cd69fb68138c150fe7400a9ccb98c2108d2dc8f0
cptntz2119 <95147931+cptntz2119@users.noreply.github.com>
2021-12-14 16:38:11 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var  p = document.getElementById("principal").value;
+    var  r = document.getElementById("rate").value;
+	var  n = document.getElementById("years").value;
+	var year = new Date().getFullYear()+parseInt(n);
+	
+	var i=r/100;
+	var y=p*i*n;
+	
+	
+		 
+		
+		if(p ==0){
+			 alert("Enter a positive number");
+			}
+		else{
+			document.getElementById("result").innerHTML= "If you deposit <mark>"+p+"</mark>,\<br\>at an interest rate of <mark>"+r+"</mark>%\<br\>You will receive an amount of <mark>"+y+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+		}
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("ratePrompt").innerText=rateval+"%";
+}
+ 
