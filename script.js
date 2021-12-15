f1b4a47ac5e68614f52d7f26e52acfa07d657ab8
cptntz2119 <95147931+cptntz2119@users.noreply.github.com>
2021-12-14 17:21:06 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function compute()
 		
 		if(p ==0){
 			 alert("Enter a positive number");
+			 location.reload();
 			}
 		else{
 			document.getElementById("result").innerHTML= "If you deposit <mark>"+p+"</mark>,\<br\>at an interest rate of <mark>"+r+"</mark>%\<br\>You will receive an amount of <mark>"+y+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
